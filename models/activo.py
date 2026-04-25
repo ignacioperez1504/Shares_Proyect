b28a329552d5714d1c8964566311f8fde6919f76
mariapau32021 <mariapaulatorresgutierrez@gmail.com>
2026-04-24 22:08:03 -0500

    Correct constructor method name in Activo class
    Fix constructor method name from _int_ to __init__.

--- models/activo.py
+++ models/activo.py
@@ -1,8 +1,7 @@
 # models/activo.py
 class Activo:
-    def __init__(self, nombre, valor_actual):
+    def _int_(self, nombre, valor_actual):
         self.nombre = nombre
         self.valor_actual = valor_actual
-
     def actualizar_valor(self, nuevo_valor):
         self.valor_actual = nuevo_valor
