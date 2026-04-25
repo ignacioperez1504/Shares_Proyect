c066808605d82ef55804ebd3d43ace1cbe6d2364
mariapau32021 <mariapaulatorresgutierrez@gmail.com>
2026-04-24 22:22:02 -0500

    Refactor Accion class and add new methods
    Refactor Accion class to fix initialization and add methods for updating value and calculating profit.

--- models/accion.py
+++ models/accion.py
@@ -1,16 +1,37 @@
 # models/accion.py
 from models.activo import Activo
+import yfinance as yf
 
 class Accion(Activo):
-    def __init__(self, ticker, cantidad, precio_compra, valor_actual, dividendos=0):
-        super().__init__(ticker, valor_actual)
+    def __init__(self, ticker, cantidad, precio_compra)
+        super().__init__(ticker)
+        if cantidad <= 0;
+            raise ValueError("La cantidad debe ser mayor a 0")
         self.ticker = ticker
         self.cantidad = cantidad
         self.precio_compra = precio_compra
-        self.dividendos = dividendos
+        self.dividendos = 0
+    def actualizar_valor(self):
+        data + yf.Ticker(self.ticker)
+        hist = data.history(period="1d")
+        if not hist.empty:
+            self.valor_actual = hist['Close'].iloc[-1]
 
     def calcular_valor(self):
         return self.cantidad * self.valor_actual
 
     def aplicar_dividendos(self, valor):
-        self.dividendos += valor
+        if valor > 0:
+            self._dividendos += valor
+
+    def calcular_ganancia(self):
+        return (self._valor_actual - self._precio_compra) * self._cantidad
+
+    def get_ticker(self):
+        return self._ticker
+
+    def get_cantidad(self):
+        return self._cantidad
+
+    def get_dividendos(self):
+        return self._dividendos
